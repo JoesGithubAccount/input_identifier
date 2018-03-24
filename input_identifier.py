4b2bfcddada93b60e244a823962627d42b9ab9dc
joesgithubaccount <joseph_lawson@hotmail.co.uk>
2018-03-24 17:15:59 +0000

    Rudimentary term identification added

--- input_identifier.py
+++ input_identifier.py
@@ -4,18 +4,39 @@ with open("test_data.txt", 'r') as f:
 	test = f.read().splitlines()
 
 #the chromosomal location format follows the format "number(s) letter numbers"
-locus_search = re.compile(r'^\d{1,2}(Q|q|p|P)\d*.\d*')
-#the Genbank accession number follows the format "letter5number/2letter6number(nucleotide) 3letter5number(protein)"
-acc_search = re.compile(r'^')
+loc_search = re.compile(r'^\d{1,2}(Q|q|p|P)\d*.\d*$')
+#the Genbank accession number follows the format "letter5number/2letter6number" for old and new respectively
+gao_search = re.compile(r'^[a-zA-Z]{1}\d{5}[.]?\d*$')
+gan_search = re.compile(r'^[a-zA-Z]{2}\d{6}[.]?\d*$')
 #gene identifier seems to be 4 characters starting with letters
-gi_search = re.compile(r'^')
+gid_search = re.compile(r'^')
 #protein product name follows much looser characteristics "else"
 ppn_search = re.compile(r'^')
 
 for line in test:
 	#print(line)
-	thing = locus_search.search(line)
+	thing = loc_search.search(line)
 	if thing != None:
 		print(thing, "chromosomal location")
+	else:
+		pass
+	thing = gao_search.search(line)
+	if thing != None:
+		print(thing, "gene accession (old)")
+	else:
+		pass
+	thing = gan_search.search(line)
+	if thing != None:
+		print(thing, "gene accession (new)")
+	else:
+		pass
+	thing = gid_search.search(line)
+	if thing != None:
+		print(thing, "gene id")
+	else:
+		pass
+	thing = ppn_search.search(line)
+	if thing != None:
+		print(thing, "protein product name")
 	else:
 		print("No match")
